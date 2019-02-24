6c627e4b57c450e963f09f263f3a7b02da6cb2ad
Kaishiyoku <dmp@nfze.de>
2019-02-24 15:13:57 +0100

    added Laravel Horizon snapshot scheduling command (2)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('horizon:snapshot')->everyFiveMinutes()
+        $schedule->command('horizon:snapshot')->everyFiveMinutes();
 
         $schedule->command(YuGiOhCrawlCards::class)->weeklyOn(1, '03:00');
     }
