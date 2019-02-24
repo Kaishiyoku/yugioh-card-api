6ecff5d7f004d3d4594861a6ad3daa4e764c15c5
Kaishiyoku <dmp@nfze.de>
2019-02-24 01:34:25 +0100

    closed #4 scheduling crawler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\YuGiOhCrawlCards;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,8 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command(YuGiOhCrawlCards::class)->weeklyOn(1, '03:00');
     }
 
     /**
