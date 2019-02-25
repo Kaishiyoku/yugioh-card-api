766ce51328b18ee3ba6a2c231afbe5891eb4839f
Kaishiyoku <dmp@nfze.de>
2019-02-25 14:05:49 +0100

    adjusted welcome page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,13 +7,13 @@
 
     <ul>
         <li>
-            {{ Html::linkRoute('api.cards.index') }}
+            {{ Html::linkRoute('api.cards.index', route('api.cards.index', null, false)) }}
         </li>
         <li>
-            {{ Html::linkRoute('api.cards.get_card_from_set', null, ['srl-g029']) }}
+            {{ Html::linkRoute('api.cards.get_card_from_set', route('api.cards.get_card_from_set', ['srl-g029'], false), ['srl-g029']) }}
         </li>
         <li>
-            {{ Html::linkRoute('api.cards.search', null, ['relinquished']) }}
+            {{ Html::linkRoute('api.cards.search', route('api.cards.search', ['relinquished'], false), ['relinquished']) }}
         </li>
     </ul>
 
@@ -21,10 +21,13 @@
 
     <ul>
         <li>
-            {{ Html::linkRoute('api.sets.index') }}
+            {{ Html::linkRoute('api.sets.index', route('api.sets.index', null, false)) }}
         </li>
         <li>
-            {{ Html::linkRoute('api.sets.search', null, ['srl']) }}
+            {{ Html::linkRoute('api.sets.show', route('api.sets.show', ['srl'], false), ['srl']) }}
+        </li>
+        <li>
+            {{ Html::linkRoute('api.sets.search', route('api.sets.search', ['srl'], false), ['srl']) }}
         </li>
     </ul>
 
@@ -32,13 +35,13 @@
 
     <ul>
         <li>
-            {{ Html::linkRoute('api.statistics.index') }}
+            {{ Html::linkRoute('api.statistics.index', route('api.statistics.index', null, false)) }}
         </li>
 
         <li>
-            {{ Html::linkRoute('api.home.version') }}
+            {{ Html::linkRoute('api.home.version', route('api.home.version', null, false)) }}
         </li>
 
-        <li>{{ Html::linkRoute('api.home.health_check') }}</li>
+        <li>{{ Html::linkRoute('api.home.health_check', route('api.home.health_check', null, false)) }}</li>
     </ul>
 @endsection
